2a4038c2157dd9d542d61525325f74f08c997a1f
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-05-20 09:23:15 +0700

    Update dashboard.php

--- application/views/home/dashboard.php
+++ application/views/home/dashboard.php
@@ -6,71 +6,84 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url() ?>node_modules/bootstrap/dist/css/bootstrap.css">
+    <link rel="stylesheet" href="<?= base_url() ?>node_modules/bootstrap/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/dashboard.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    
     <title>Dashboard</title>
   </head>
   <body>
-    <header>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-   
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Admin
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">User1</a></li>
-            <li><a class="dropdown-item" href="#">User2</a></li>
-            <li><a class="dropdown-item" href="#">User3</a></li>
-          </ul>
-        </li> 
-      </ul>
-      <form class="d-flex">
-        <button class="btn btn-outline-success" type="submit">Logout</button>
-      </form>
+  <div class="d-flex pt-5" style="background-color: #FFF !important;">
+    <div class="p-5 col-md-10 shadow mb-5 ml-container" style="border-radius: 30px;">
+        <h1>Selamat Datang <span id="nama">Admin GG</span></h1>
+
+        <div class="mt-5">
+            <div class="row">
+                <div class="col">
+                    <div class="card" style="background-color:#5DD482; border:none; border-radius: 10px;">
+                        <div class="card-body m-3">
+                            <h4>Data Pengunjung</h4>
+                            <p style="text-align: center; font-size: 76px;"><b></b></p>
+                            <h4 style="text-align: right;"> orang </h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col ms-3">
+                    <div class="card" style="background-color:#5DD482; border:none; border-radius: 10px;">
+                        <div class="card-body m-3">
+                            <h4>Jumlah Mitra</h4>
+                            <p style="text-align: center; font-size: 76px;"><b></b> </p>
+                            <h4 style="text-align: right;"> Toko </h4>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col ms-3">
+                    <div class="card" style="background-color:#5DD482; border:none; border-radius: 10px;">
+                        <div class="card-body m-3">
+                            <h4>Jumlah Kunjungan</h4>
+                            <p style="text-align: center; font-size: 76px;"><b></b> </p>
+                            <h4 style="text-align: right;"> Kunjungan </h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mt-5 lato-regular">
+                <div class="col-md-12">
+                    <div class="p-3 bg-body rounded2 p-5" style="border: 1px solid rgb(221, 221, 221); background-color: whitesmoke !important;">
+                        <div class="row ">
+                            <div class="col-md-3 text-center">
+                                <h4>Total Kasus</h4>
+                                <h2 class=" lato-bold mt-3 mb-3" id="totalKasus">4.247.320</h2>
+                                <h6>+20.748 kasus</h6>
+                            </div>
+                            <div class="col-md-3 text-center">
+                                <h4>Positif</h4>
+                                <h2 class="text-warning lato-bold mt-3 mb-3" id="positif">124.632</h2>
+                                <h6>0,10% dari total kasus</h6>
+                            </div>
+                            <div class="col-md-3 text-center">
+                                <h4>Sembuh</h4>
+                                <h2 class="text-success lato-bold mt-3 mb-3" id="sembuh">4.092.586</h2>
+                                <h6>96,52% dari total kasus</h6>
+                            </div>
+                            <div class="col-md-3 text-center">
+                                <h4>Meninggal</h4>
+                                <h2 class="text-danger lato-bold mt-3 mb-3" id="meninggal">143.519</h2>
+                                <h6>3,38% dari total kasus</h6>
+                            </div>
+                            <div class="col-md-12 text-center mt-5">
+                                <img src="assets/image/flag.png ">
+                                <span class="ps-2">Data oleh <a href="">a.com</a>. Diperbarui jam 19:39 pada tanggal 01 Januari 2022</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</nav>
+</div>
 
-</header>
-
-    
-
-    <div class="card">
-        <div class="col-md5">
-            <div class="row">
-            <h5 class="card-title">Total Items</h5>
-            <h1 class="card-text">30000</h1>
-            <a class="text-muted">2 days ago</a>
-            </div>
-        </div>
-        <div class="col-md5">
-            <div class="row">
-            <h5 class="card-title">Total Items</h5>
-            <h1 class="card-text">30000</h1>
-            <a class="text-muted">2 days ago</a>
-            </div>
-        </div>
-        
-    </div> 
-    
-
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
   </body>
 </html>
