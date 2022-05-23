560205fbef2fc9c98422facbb7e18eb0fc4faf50
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-05-23 09:31:03 +0700

    Modal

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,12 +14,15 @@
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/stock.css">
 
     <!-- Data Tables -->
-    <link rel="stylesheet" type="text/css" media="screen"
-        href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.js"> </script>
     <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"> </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <!-- Modal -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 
 
