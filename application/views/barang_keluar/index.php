c7671b5b6f704ad5280b97442a88b37c6df91d54
izalmfrds <rizalmaidanytb@gmail.com>
2022-05-23 09:53:55 +0700

    page in and out

--- application/views/barang_keluar/index.php
+++ application/views/barang_keluar/index.php
@@ -71,12 +71,51 @@
             </div>
         </nav> -->
 
-        <div class="col">
-            <!--  ISI DISINI  -->
-            BARANG KELUAR
+<div class="col">
+    <!--  ISI DISINI  -->
+    <table id="warehouse" class="table table-striped">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>Item Code</th>
+                <th>Item Name</th>
+                <th>Item Spec</th>
+                <th>UoM</th>
+                <th>Location</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- ISI KONFIG BACKEND DISINI -->
+
+        </tbody>
+    </table>
+</div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                ...
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
         </div>
     </div>
-
+</div>
 </body>
+<script>
+$(document).ready(function() {
+    $('#warehouse').DataTable();
+});
+</script>
 
 </html>
