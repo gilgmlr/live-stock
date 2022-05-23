23447c5103bf7d512cd67f963b315eb83018aca6
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-05-23 08:34:27 +0700

    Update live_stock.sql

--- database/live_stock.sql
+++ database/live_stock.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Bulan Mei 2022 pada 02.33
+-- Waktu pembuatan: 23 Bulan Mei 2022 pada 03.20
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 7.4.26
 
@@ -24,22 +24,48 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `data_barang`
+--
+
+CREATE TABLE `data_barang` (
+  `item_code` varchar(20) NOT NULL,
+  `item_name` varchar(200) NOT NULL,
+  `item_specification` varchar(200) NOT NULL,
+  `uom` int(11) NOT NULL,
+  `location` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `data_barang`
+--
+
+INSERT INTO `data_barang` (`item_code`, `item_name`, `item_specification`, `uom`, `location`) VALUES
+('012-2087-287', 'Thinner TCI', 'Thinner Specification Here', 120, '01-933-485-3'),
+('012-2087-287', 'Thinner TCI', 'Thinner Specification Here', 120, '01-933-485-3'),
+('012-2087-287', 'Thinner TCI', 'Thinner Specification Here', 120, '01-933-485-3'),
+('012-2987-2834', 'ABB DC Drivers', 'ABB DC Specification Here', 394, '02-465-483-1'),
+('012-2987-2834', 'ABB DC Drivers', 'ABB DC Specification Here', 394, '02-465-483-1'),
+('012-2987-2834', 'ABB DC Drivers', 'ABB DC Specification Here', 394, '02-465-483-1');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `user`
 --
 
 CREATE TABLE `user` (
-  `nip` varchar(20) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `password` varchar(20) NOT NULL
+  `nip` varchar(50) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`nip`, `name`, `password`) VALUES
-('123', 'Gilang', '123'),
-('1302194089', 'Gilang Gumelar', '1302194089');
+INSERT INTO `user` (`nip`, `name`, `username`, `password`) VALUES
+('1302194089', 'Gilang', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -65,12 +91,6 @@ INSERT INTO `warehouse` (`id`, `name`, `address`, `total_items`) VALUES
 --
 -- Indexes for dumped tables
 --
-
---
--- Indeks untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`nip`);
 
 --
 -- Indeks untuk tabel `warehouse`
