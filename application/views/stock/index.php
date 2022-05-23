b16884af6145808184bcbae3b457de8690ba2838
izalmfrds <rizalmaidanytb@gmail.com>
2022-05-23 09:31:33 +0700

    up

--- application/views/stock/index.php
+++ application/views/stock/index.php
@@ -43,29 +43,8 @@
                         <td>
                             <button type="button" class="btn btn-warning btn-sm" data-toggle="modal"
                                 data-target="#editModal">Edit</button>
-                            <div class="modal" tabindex="-1">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                        <div class="modal-header">
-                                            <h5 class="modal-title">Modal title</h5>
-                                            <button type="button" class="btn-close" data-bs-dismiss="modal"
-                                                aria-label="Close"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <p>Modal body text goes here.</p>
-                                        </div>
-                                        <div class="modal-footer">
-                                            <button type="button" class="btn btn-secondary"
-                                                data-bs-dismiss="modal">Close</button>
-                                            <button type="button" class="btn btn-primary">Save changes</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-
                         </td>
                     </tr>
-
                     <?php
                                 $i++;
                                 }
@@ -74,8 +53,27 @@
             </table>
         </div>
     </div>
+    <div class="modal" id=editModal tabindex="-1">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </body>
+
+
 <script>
 $(document).ready(function() {
     $('#warehouse').DataTable();
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -18,7 +18,6 @@
         href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.5.1.js"> </script>
     <script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"> </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 
 
