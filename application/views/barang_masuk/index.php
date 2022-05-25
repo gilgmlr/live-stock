e09db2d6bca0793f8d9f27700a8ab8293be053b8
izalmfrds <rizalmaidanytb@gmail.com>
2022-05-25 10:37:10 +0700

    tambahbarang page

--- application/views/barang_masuk/index.php
+++ application/views/barang_masuk/index.php
@@ -1,44 +1,152 @@
 <div class="container">
-    <div class="card justyfy-content-center">
-        <form>
-            <div class="row">
-                <div class="col">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">No GR</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="colFormLabel">
+    <div class="card">
+        <!-- <form>
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-2 col-form-label">No GR</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-4 col-form-label">Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
                     </div>
                 </div>
-                <div class="col">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">No GR</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="colFormLabel">
+                <div class="row justify-content-between">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-4 col-form-label">Date</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Dept Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
                     </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">No GR</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="colFormLabel">
-                    </div>
-                </div>
-                <div class="col">
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">No GR</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="colFormLabel">
-                    </div>
-                    <label for="colFormLabel" class="col-sm-2 col-form-label">No GR</label>
-                    <div class="col-sm-4">
-                        <input type="email" class="form-control" id="colFormLabel">
+                <div class="row justify-content-end">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Section Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
                     </div>
                 </div>
 
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="floatingTextarea">Activity Description</label>
+                        <textarea class="form-control" placeholder="Leave a description here"
+                            id="floatingTextarea"></textarea>
+                    </div>
+                </div>
+
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-4 col-form-label">Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                </div>
+
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="floatingTextarea">Items Description</label>
+                        <textarea class="form-control" placeholder="Leave a description here"
+                            id="floatingTextarea"></textarea>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-md-4">
+                            <label for="inputEmail3" class="col-sm-12 col-form-label">Approved by</label>
+                            <input type="email" class="form-control " id="inputText">
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-md-4">
+                            <label for="inputEmail3" class="col-sm-12 col-form-label">Issued by</label>
+                            <input type="email" class="form-control " id="inputText">
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-md-4">
+                            <label for="inputEmail3" class="col-sm-12 col-form-label">Recived by</label>
+                            <input type="email" class="form-control " id="inputText">
+                        </div>
+                    </div>
+                </div>
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-12 col-form-label">Request Qty</label>
+                        <input type="email" class="form-control " id="inputText">
+                    </div>
+                </div>
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-12 col-form-label">Issued Qty</label>
+                        <input type="email" class="form-control " id="inputText">
+                    </div>
+                </div>
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-12 col-form-label">UoM</label>
+                        <input type="email" class="form-control " id="inputText">
+                    </div>
+                </div>
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-12 col-form-label">Reason Code</label>
+                        <input type="email" class="form-control " id="inputText">
+                    </div>
+                </div>
+                <div class="row justify-content-start">
+                    <div class="col-md-4">
+                        <label for="inputEmail3" class="col-sm-12 col-form-label">Cost Center</label>
+                        <input type="email" class="form-control " id="inputText">
+                    </div>
+                </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <div class="col-md-6">
-                        <label for="inputEmail4" class="form-label">Email</label>
-                        <input type="email" class="form-control" id="inputEmail4">
+        </form> -->
+
+        <form>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-6">
+                        <label for="inputEmail3" class="col-sm-2 col-form-label">No GR</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Date</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Dept Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Section Requestor</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                    <div class="col-sm-12">
+                        <label for="floatingTextarea">Activity Description</label>
+                        <textarea class="form-control" placeholder="Leave a description here"
+                            id="floatingTextarea"></textarea>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Item Number</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <div class="row">
+                            <label for="floatingTextarea">Items Description</label>
+                            <textarea class="form-control" placeholder="Leave a description here"
+                                id="floatingTextarea"></textarea>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Request by</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Approved by</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Issued by</label>
+                        <input type="email" class="form-control" id="inputText">
+                        <label for="inputEmail3" class="col-sm-6 col-form-label">Recived by</label>
+                        <input type="email" class="form-control" id="inputText">
+                    </div>
+                    <div class="row justify-content-between">
+                        <button type="button" class="btn btn-primary">Tambah Items</button>
+                        <button type="button" class="btn btn-primary">Batal</button>
+                        <button type="button" class="btn btn-primary">Simpan</button>
                     </div>
                 </div>
             </div>
