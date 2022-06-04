87264d7c3e3a77e12398d11aba31728a457742e3
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-04 20:57:55 +0700

    Update result.php

--- application/views/search/result.php
+++ application/views/search/result.php
@@ -19,19 +19,16 @@
             foreach ($items as $data) { 
         ?>
                 
-        <div class="col-md-3 px-2">
-            <div class="card shadow p-3 mb-5 bg-white rounded">
-                <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>"
-                    class="card-img-top justify-content-center" alt="image" style="max-height: 300px;">
-                <div class="card-body">
-                    <div class="card-text"><b>Code : <?= $data->item_code ?></b></div>
-                    <div class="card-text">Name : <?= $data->name ?></div>
-                    <p></p>
-                    <a href="<?php echo base_url() . "search/view_detail" ?>" class="btn btn-primary" style=margin:0;>
-                        Details
-                    </a>
+        <div class="col-md-2 px-2">
+            <a href="<?= base_url() . "search/view_detail" ?>">
+                <div class="card shadow p-2 mb-2 bg-white rounded">
+                    <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>" class="card-img-top justify-content-center" alt="">
+                    <div class="card-body">
+                        <p class="card-text" style="font-size:16px;"><b><?= $data->item_code ?></b></p>
+                        <p class="card-text" style="font-size:12px;"><?= $data->name ?></p>
+                    </div>
                 </div>
-            </div>
+            </a>
         </div>
 
         <?php } ?>
