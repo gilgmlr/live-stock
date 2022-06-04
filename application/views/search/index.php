29712c7e801965eb60ac951e14ca682bd94ae275
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-04 17:08:31 +0700

    Update index.php

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -20,7 +20,7 @@
                 <div class="col-md-3 px-2">
                     <div class="card shadow p-3 mb-5 bg-white rounded">
                         <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>"
-                            class="card-img-top justify-content-center" alt="image">
+                            class="card-img-top justify-content-center" alt="image" style="max-height: 300px;">
                         <div class="card-body">
                             <div class="card-text"><b>Code : <?= $data->item_code ?></b></div>
                             <div class="card-text">Name : <?= $data->name ?></div>
