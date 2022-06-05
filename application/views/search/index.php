9031ec53daf1679341155ed2f7b2b646d0ee1fe7
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-05 12:55:12 +0700

    Update index.php

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -18,7 +18,7 @@
                 ?>
                 
                 <div class="col-md-2 px-2">
-                    <a href="<?= base_url() . "search/view_detail" ?>" data-bs-toggle="modal" data-bs-target="#detail">
+                    <a href="<?= base_url() . "search/view_detail" ?>" data-bs-toggle="modal" data-bs-target="#detail<?= $data->item_code ?>">
                         <div class="card shadow p-2 mb-2 bg-white rounded">
                             <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>" class="card-img-top justify-content-center" alt="Not Found!">
                             <div class="card-body">
@@ -29,13 +29,8 @@
                     </a>
                 </div>
 
-                <?php } ?>
-            </div>
-        </div>
-    </div>
-
 <!-- Modal -->
-<div class="modal fade" id="detail" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="detail<?= $data->item_code ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style=max-width:60%>
         <div class="card-body">
             <div class="modal-content">
@@ -47,14 +42,17 @@
                     <div class="row">
                         <div class="col">
                             <div class="card shadow p-3 mb-5 bg-white rounded">
-                                <img src="<?= base_url() . 'assets/catalog/' . $items[1]->image ?>"
+                                <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>"
                                     class="card-img-top justify-content-center" alt="Not Found!">
                             </div>
                         </div>
                         <div class="col">
                             <div class="card shadow p-3 mb-5 bg-white rounded">
                                 <div class="card-body">
-                                    description here
+                                    Code         : <?= $data->item_code ?><br>
+                                    Name         : <?= $data->name ?><br>
+                                    Specification: <?= $data->specification ?><br>
+                                    <!-- location: <?= $data->name ?><br> -->
                                 </div>
                             </div>
                         </div>
@@ -68,6 +66,11 @@
     </div>
 </div>
 <!-- End Modal  -->
+
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 
 </body>
 
