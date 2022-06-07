93348f0260da03ba3df1da384655c13af3bde856
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-07 14:08:28 +0700

    Update Dashboard.php

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,18 +6,18 @@ class Dashboard extends CI_Controller
     {
         parent::__construct();
 
-        $this->load->model('M_Dashboard');
+        $this->load->model('M_CRUD');
         $this->load->helper('date');
     }
 
     public function index()
     {
-        $data['warehouse'] = $this->M_Dashboard->getDataWarehouse()->result();
-        $data['total_items'] = $this->M_Dashboard->get_total_items();
-        $data['total_received'] = $this->M_Dashboard->get_total_received();
-        $data['total_issue'] = $this->M_Dashboard->get_total_issue();
-        $data['total_lending'] = $this->M_Dashboard->get_total_lending();
-        $data['last_update'] = $this->M_Dashboard->get_Last_Update()->result();
+        $data['warehouse'] = $this->M_CRUD->get_data('warehouse')->result();
+        $data['total_items'] = $this->M_CRUD->get_total('items');
+        $data['total_received'] = $this->M_CRUD->get_total('received');
+        $data['total_issue'] = $this->M_CRUD->get_total('issued');
+        $data['total_lending'] = $this->M_CRUD->get_total('lending');
+        $data['last_update'] = $this->M_CRUD->get_data_sort('history', 'id', 'desc')->result();
         $data['judul'] = 'Dashboard';
 
         $this->load->view('template/header', $data);
