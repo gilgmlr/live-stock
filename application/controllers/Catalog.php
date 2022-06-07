d088c4b21902a954b5a36e5f80b54f3c14cfbf8f
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-07 14:13:08 +0700

    Update Catalog.php

--- application/controllers/Catalog.php
+++ application/controllers/Catalog.php
@@ -6,13 +6,13 @@ class Catalog extends CI_Controller
     {
         parent::__construct();
 
-        $this->load->model(array('M_CRUD', 'M_Catalog'));
+        $this->load->model('M_CRUD');
     }
 
     public function index()
     {
         $data['judul'] = 'Catalog';
-        $data['items'] = $this->M_Catalog->getDataItems()->result();
+        $data['items'] = $this->M_CRUD->get_data('items')->result();
 
         $this->load->view('template/header', $data);
         $this->load->view('catalog/index', $data);
