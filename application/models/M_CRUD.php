3b14342e881b9d5aed28f844f0f650dc07286ddd
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-07 14:09:12 +0700

    Tambah Model CRUD

---
+++ application/models/M_CRUD.php
@@ -0,0 +1,72 @@
+<?php
+
+class M_CRUD extends CI_Model
+{
+	function get_data($table){
+		return $this->db->get($table);
+	}
+
+	function get_data_where($table, $where){		
+		return $this->db->get_where($table, $where);
+	}
+
+	public function get_join($table, $join, $matching)
+	{
+		$this->db->select('*');
+		$this->db->from($table);
+		$this->db->join($join, $matching);
+		$query = $this->db->get();
+
+		return $query;
+	}
+
+	public function get_join2($table, $join1, $matching1, $join2, $matching2)
+	{
+		$this->db->select('*');
+		$this->db->from($table);
+		$this->db->join($join1, $matching1);
+		$this->db->join($join2, $matching2);
+		$query = $this->db->get();
+
+		return $query;
+	}
+
+	public function get_join3($table, $join1, $matching1, $join2, $matching2, $join3, $matching3)
+	{
+		$this->db->select('*');
+		$this->db->from($table);
+		$this->db->join($join1, $matching1);
+		$this->db->join($join2, $matching2);
+		$this->db->join($join3, $matching3);
+		$query = $this->db->get();
+
+		return $query;
+	}
+
+	function get_data_sort($table, $orderby, $sortKeys){
+		$this->db->select('*');
+		$this->db->from($table);
+		$this->db->order_by($orderby, $sortKeys);
+
+		$query = $this->db->get();
+		return $query;
+	}
+ 
+	function input_data($table, $data){
+		$this->db->insert($table, $data);
+	}
+ 
+	function hapus_data($table, $where){
+		$this->db->where($where);
+		$this->db->delete($table);
+	}
+ 
+	function update_data($table, $data, $where){
+		$this->db->where($where);
+		$this->db->update($table,$data);
+	}
+
+	function get_total($table) {
+		return $this->db->get($table)->num_rows();
+	}
+}
