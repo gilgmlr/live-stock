2c6a865ff31e594859f83304d70542832333352b
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-08 09:41:10 +0700

    material issue

--- application/views/issue/material_issue.php
+++ application/views/issue/material_issue.php
@@ -1,7 +1,7 @@
 <div class="container" style="margin-top: 120px;">
     <div class="cards shadow p-1 mb-5 bg-white rounded">
         <center>
-            <h4 style="font-weight:bold"> Form Material Issue</h4>
+            <h4 style="font-weight:bold;"> Form Material Issue</h4>
         </center>
         <div class=" card-body">
             <form action="<?= base_url() ?>received/addReceived" method="POST">
@@ -9,21 +9,28 @@
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-sm-6">
-                            <label for="" class="col-sm-6 col-form-label">GR Number</label>
-                            <input type="text" class="form-control" id="received_code" name="received_code" required>
-                            <label for="" class="col-sm-6 col-form-label">Arrival Date</label>
+                            <label for="" class="col-sm-6 col-form-label">MI No</label>
+                            <select class="form-select form-control" aria-label=".form-select-lg example" id="uom"
+                                name="uom" required>
+                                <option selected>-- Select --</option>
+                                <?php foreach ($uom as $data) { ?>
+                                <option value="<?= $data->uom_code ?>"><?= $data->name ?></option>
+                                <?php $i++; } ?>
+                            </select> <label for="" class="col-sm-6 col-form-label">Entry Date</label>
                             <input type="date" class="form-control" id="arrival_date" name="arrival_date" required>
-                            <label for="" class="col-sm-6 col-form-label">PO Number</label>
+                            </select> <label for="" class="col-sm-6 col-form-label">Posting Date</label>
+                            <input type="date" class="form-control" id="arrival_date" name="arrival_date" required>
+                            <label for="" class="col-sm-6 col-form-label">Applicant</label>
                             <input type="text" class="form-control" id="po_number" name="po_number" required>
-                            <label for="" class="col-sm-6 col-form-label">Vendor Name</label>
+                            <label for="" class="col-sm-6 col-form-label">Dept No</label>
                             <input type="text" class="form-control" id="vendor_name" name="vendor_name" required>
+                            <label for="" class="col-sm-6 col-form-label">Project No</label>
+                            <input type="text" class="form-control" id="vendor_name" name="vendor_name" required>
+                            <label for="" class="col-sm-6 col-form-label">Memo</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea4" rows="3"></textarea>
                         </div>
                         <div class="col-sm-6">
-                            <label for="" class="col-sm-6 col-form-label">Item Code</label>
-                            <input type="text" class="form-control" id="item_code" name="item_code" required>
-                            <label for="" class="col-sm-6 col-form-label">Qty</label>
-                            <input type="text" class="form-control" id="qty" name="qty" required>
-                            <label for="" class="col-sm-6 col-form-label">UoM</label>
+                            <label for="" class="col-sm-6 col-form-label">Item No</label>
                             <select class="form-select form-control" aria-label=".form-select-lg example" id="uom"
                                 name="uom" required>
                                 <option selected>-- Select --</option>
@@ -31,8 +38,24 @@
                                 <option value="<?= $data->uom_code ?>"><?= $data->name ?></option>
                                 <?php $i++; } ?>
                             </select>
+                            <label for="" class="col-sm-6 col-form-label">WH</label>
+                            <select class="form-select form-control" aria-label=".form-select-lg example" id="uom"
+                                name="uom" required>
+                                <option selected>-- Select --</option>
+                                <?php foreach ($uom as $data) { ?>
+                                <option value="<?= $data->uom_code ?>"><?= $data->name ?></option>
+                                <?php $i++; } ?>
+                            </select>
+                            <label for="" class="col-sm-6 col-form-label">Transaction Qty</label>
+                            <input type="text" class="form-control" id="item_code" name="item_code" required>
+                            <label for="" class="col-sm-6 col-form-label">Reference</label>
+                            <input type="text" class="form-control" id="qty" name="qty" required>
 
-                            <label for="" class="col-sm-6 col-form-label">Location</label>
+                            <label for="" class="col-sm-6 col-form-label">Reason</label>
+                            <input type="text" class="form-control" id="location" name="location" required>
+                            <label for="" class="col-sm-6 col-form-label">Desc</label>
+                            <input type="text" class="form-control" id="location" name="location" required>
+                            <label for="" class="col-sm-6 col-form-label">Create by</label>
                             <input type="text" class="form-control" id="location" name="location" required>
                         </div>
                     </div>
