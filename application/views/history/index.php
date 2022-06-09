88d01f48a8a48155cef118a7ccf7f1026ff0fde0
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-09 11:31:48 +0700

    history

--- application/views/history/index.php
+++ application/views/history/index.php
@@ -30,7 +30,7 @@
                                         <!-- <button type="button" class="btn btn-warning btn-sm"
                                             style="margin:0px; height:35px;" data-bs-toggle="modal"
                                             data-bs-target="#edit<?=$i ?>">View</button> -->
-                                        <a href="" data-bs-toggle="modal" data-bs-target="#history<?=$i ?>"><i
+                                        <a href="" data-bs-toggle="modal" data-bs-target="#history"><i
                                                 class="fa-solid fa-eye"></i></a>
                                     </td>
                                 </tr>
@@ -51,55 +51,25 @@
     </div>
 </div>
 
-<!-- Modal -->
+<!-- MODAL HISTORY -->
 <div class="modal fade" id="history" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style=max-width:60%>
-        <div class="card">
-            <div class="card-body">
-                <div class="modal-content">
-                    <div class="modal-header" style=background-color:#D7A86E>
-                        <h5 class="modal-title" id="exampleModalLabel">Form Edit Barang</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <form>
-                            <div class="mb-3">
-                                <label for="exampleInputno" class="form-label">No</label>
-                                <input type="Text" class="form-control" id="exampleInputno" aria-describedby="no">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleItemCode" class="form-label">Item Code</label>
-                                <input type="Text" class="form-control" id="exampleItemCode">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleItemName" class="form-label">Item Name</label>
-                                <input type="Text" class="form-control" id="exampleItemName">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleItemSpec" class="form-label">Item Spec</label>
-                                <input type="Text" class="form-control" id="exampleItemSpec">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleUoM" class="form-label">UoM</label>
-                                <input type="Text" class="form-control" id="exampleUoM">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleLocation" class="form-label">Location</label>
-                                <input type="Text" class="form-control" id="exampleLocation">
-                            </div>
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" style=max-width:80%>
+        <div class="modal-content">
+            <div class="modal-header" style="background-color: #563d7c">
+                <h5 class="modal-title" style="color: gold" id="exampleModalLabel"> DETAIL HISTORY
+                </h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
 
-                        </form>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-success">Save changes</button>
-                        </div>
-                    </div>
-
-                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
             </div>
         </div>
     </div>
 </div>
+<!-- End Modal -->
 </body>
 <script>
 $(document).ready(function() {
