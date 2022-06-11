1d74c125a61b2cb11d25bb81e478471cd3c6c1a8
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-11 21:19:32 +0700

    fix bug pagination

--- application/controllers/Catalog.php
+++ application/controllers/Catalog.php
@@ -1,4 +1,4 @@
-l<?php
+<?php
 
 class Catalog extends CI_Controller
 {
@@ -15,28 +15,24 @@ class Catalog extends CI_Controller
         // Pagination
         $config['base_url'] = base_url().'catalog/index';
         $config['total_rows'] = $this->M_CRUD->count_row('items');
-        $config['per_page'] = 12;
+        $config['per_page'] = 18;
 
         // Styling
         $config['full_tag_open'] = '<nav><ul class="pagination justify-content-center">';
         $config['full_tag_close'] = '</ul></nav>';
 
-        $config['first_link'] = 'First';
         $config['first_tag_open'] = '<li class="page-item">';
         $config['first_tag_close'] = '</li>';
-        
-        $config['last_link'] = 'Last';
+
         $config['last_tag_open'] = '<li class="page-item">';
         $config['last_tag_close'] = '</li>';
-        
-        $config['next_link'] = '&raquo';
+
         $config['next_tag_open'] = '<li class="page-item">';
         $config['next_tag_close'] = '</li>';
         
-        $config['prev_link'] = '&laquo';
         $config['prev_tag_open'] = '<li class="page-item">';
         $config['prev_tag_close'] = '</li>';
-        
+
         $config['cur_tag_open'] = '<li class="page-item active"><a class="page-link" href="#">';
         $config['cur_tag_close'] = '</a></li>';
         
--- application/views/catalog/index.php
+++ application/views/catalog/index.php
@@ -71,9 +71,17 @@
                 <!-- End Modal  -->
 
                 <?php } ?>
-                
                 <div class="col-12">
-                <?= $this->pagination->create_links(); ?>
+                    <!-- <nav>
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav> -->
+                    <?= $this->pagination->create_links(); ?>
                 </div>
             </div>
         </div>
