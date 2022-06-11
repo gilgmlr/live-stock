0eaf8e58bbfcbee427d9e29cd1bd7b7186ab7d8e
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-11 21:37:08 +0700

    Update index.php

--- application/views/catalog/index.php
+++ application/views/catalog/index.php
@@ -12,7 +12,7 @@
                 </div>
             </form>
 
-            <div class="container d-flex align-content-start flex-wrap" style="margin-top: 0;">
+            <div class="container d-flex align-content-start flex-wrap justify-content-center" style="margin-top: 0;">
                 <?php
                     foreach ($items as $data) { 
                 ?>
@@ -20,7 +20,7 @@
                 <div class="col-md-2 px-2">
                     <a href="" data-bs-toggle="modal" data-bs-target="#detail<?= $data->item_code ?>">
                         <div class="card shadow p-2 mb-2 bg-white rounded">
-                            <img src="<?= 'assets/catalog/' . $data->image ?> " class="img-fluid" alt="Not Found!"
+                            <img src="<?= base_url() . 'assets/catalog/' . $data->image ?>" class="img-fluid" alt="Not Found!"
                                 style="height: 194px; width:259px">
                             <div class="card-body">
                                 <p class="card-text" style="font-size:16px;"><b><?= $data->item_code ?></b></p>
@@ -72,15 +72,6 @@
 
                 <?php } ?>
                 <div class="col-12">
-                    <!-- <nav>
-                        <ul class="pagination justify-content-center">
-                            <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
-                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                        </ul>
-                    </nav> -->
                     <?= $this->pagination->create_links(); ?>
                 </div>
             </div>
