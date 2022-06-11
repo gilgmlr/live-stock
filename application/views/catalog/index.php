3d808b9bca668d36adc8c74cea5c050813ba761e
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-11 08:43:03 +0700

    add pagination

--- application/views/catalog/index.php
+++ application/views/catalog/index.php
@@ -72,6 +72,17 @@
                 <!-- End Modal  -->
 
                 <?php } ?>
+                <div class="col-12">
+                    <nav>
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item disabled"><a class="page-link" href="#">Previous</span></a></li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
         </div>
 
