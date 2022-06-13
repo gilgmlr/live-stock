72b0aaf13786e2bf28dda3ea2145d00bc16b73fa
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-13 14:59:21 +0700

    preview&print

---
+++ application/views/preview/print.php
@@ -0,0 +1,121 @@
+<table style="width: 100%;">
+    <tbody>
+        <tr>
+            <td style="width: 39.1566%;">
+                <div data-empty="true" style="text-align: center;"><br></div><br><br><br>
+                <div style="text-align: center;">PT CEMINDO GEMILANG Tbk</div>
+            </td>
+            <td style="width: 27.4527%;">
+                <div style="text-align: center;">GOOD ISSUE FORM</div>
+            </td>
+            <td style="width: 33.3333%;">Document No &nbsp; :<br>Version &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                :<br>Effective Date &nbsp;:</td>
+        </tr>
+    </tbody>
+</table>
+<table style="width: 100%;">
+    <tbody>
+        <tr>
+            <td style="width: 100.0000%;">&nbsp;No GIF : &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Requestor
+                &nbsp;:<br>&nbsp;Date &nbsp; &nbsp; &nbsp;: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Dept Requestor
+                :<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+                &nbsp;Section Requestor :<br><br>
+                <table style="width: 72%; margin-left: calc(14%); margin-right: calc(14%);">
+                    <tbody>
+                        <tr>
+                            <td style="width: 100.0000%;">Activities Description :<br><br></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
+<table style="width: 100%;">
+    <tbody>
+        <tr>
+            <td style="width: 8.0034%;">
+                <div style="text-align: center;">NO</div>
+            </td>
+            <td style="width: 16.4372%;">
+                <div style="text-align: center;">&nbsp;Item Name</div>
+            </td>
+            <td style="width: 20.1378%;">
+                <div style="text-align: center;">Item Description</div>
+            </td>
+            <td style="width: 18.0724%;">
+                <div style="text-align: center;">Requested Qty</div>
+            </td>
+            <td style="width: 12.9087%;">
+                <div style="text-align: center;">Issue Qty</div>
+            </td>
+            <td style="width: 5.1636%;">
+                <div style="text-align: center;">UoM</div>
+            </td>
+            <td style="width: 12.5000%;">
+                <div style="text-align: center;">Reason Code</div>
+            </td>
+            <td style="width: 9.7246%;">
+                <div style="text-align: center;">Cost Center</div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 8.0034%;"><br><br><br><br><br><br><br><br></td>
+            <td style="width: 16.4372%;"><br></td>
+            <td style="width: 20.1378%;"><br></td>
+            <td style="width: 18.0724%;"><br></td>
+            <td style="width: 12.9087%;"><br></td>
+            <td style="width: 5.1636%;"><br></td>
+            <td style="width: 12.5000%;"><br></td>
+            <td style="width: 9.7246%;"><br></td>
+        </tr>
+    </tbody>
+</table>
+<table style="width: 100%;">
+    <tbody>
+        <tr>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Requested by</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Approved by</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Issued by</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Received by</div>
+            </td>
+        </tr>
+        <tr>
+            <td style="width: 25.0000%;"><br><br><br></td>
+            <td style="width: 25.0000%;"><br></td>
+            <td style="width: 25.0000%;"><br></td>
+            <td style="width: 25.0000%;"><br></td>
+        </tr>
+        <tr>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Name &amp; Position</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Name &amp; Position</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Name &amp; Position</div>
+            </td>
+            <td style="width: 25.0000%;">
+                <div style="text-align: center;">Name &amp; Position</div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+<script>
+window.print();
+</script>
