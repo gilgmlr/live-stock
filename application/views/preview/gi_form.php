89bc0f5d1aa0089d97c4d92ef959ea7a0bcc037e
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-13 14:20:27 +0700

    Update gi_form.php

--- application/views/preview/gi_form.php
+++ application/views/preview/gi_form.php
@@ -1,121 +1,55 @@
-<table style="width: 100%;">
-    <tbody>
-        <tr>
-            <td style="width: 39.1566%;">
-                <div data-empty="true" style="text-align: center;"><br></div><br><br><br>
-                <div style="text-align: center;">PT CEMINDO GEMILANG Tbk</div>
-            </td>
-            <td style="width: 27.4527%;">
-                <div style="text-align: center;">GOOD ISSUE FORM</div>
-            </td>
-            <td style="width: 33.3333%;">Document No &nbsp; :<br>Version &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                :<br>Effective Date &nbsp;:</td>
-        </tr>
-    </tbody>
-</table>
-<table style="width: 100%;">
-    <tbody>
-        <tr>
-            <td style="width: 100.0000%;">&nbsp;No GIF : &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Requestor
-                &nbsp;:<br>&nbsp;Date &nbsp; &nbsp; &nbsp;: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Dept Requestor
-                :<br>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                &nbsp;Section Requestor :<br><br>
-                <table style="width: 72%; margin-left: calc(14%); margin-right: calc(14%);">
-                    <tbody>
-                        <tr>
-                            <td style="width: 100.0000%;">Activities Description :<br><br></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </td>
-        </tr>
-    </tbody>
-</table>
-<table style="width: 100%;">
-    <tbody>
-        <tr>
-            <td style="width: 8.0034%;">
-                <div style="text-align: center;">NO</div>
-            </td>
-            <td style="width: 16.4372%;">
-                <div style="text-align: center;">&nbsp;Item Name</div>
-            </td>
-            <td style="width: 20.1378%;">
-                <div style="text-align: center;">Item Description</div>
-            </td>
-            <td style="width: 18.0724%;">
-                <div style="text-align: center;">Requested Qty</div>
-            </td>
-            <td style="width: 12.9087%;">
-                <div style="text-align: center;">Issue Qty</div>
-            </td>
-            <td style="width: 5.1636%;">
-                <div style="text-align: center;">UoM</div>
-            </td>
-            <td style="width: 12.5000%;">
-                <div style="text-align: center;">Reason Code</div>
-            </td>
-            <td style="width: 9.7246%;">
-                <div style="text-align: center;">Cost Center</div>
-            </td>
-        </tr>
-        <tr>
-            <td style="width: 8.0034%;"><br><br><br><br><br><br><br><br></td>
-            <td style="width: 16.4372%;"><br></td>
-            <td style="width: 20.1378%;"><br></td>
-            <td style="width: 18.0724%;"><br></td>
-            <td style="width: 12.9087%;"><br></td>
-            <td style="width: 5.1636%;"><br></td>
-            <td style="width: 12.5000%;"><br></td>
-            <td style="width: 9.7246%;"><br></td>
-        </tr>
-    </tbody>
-</table>
-<table style="width: 100%;">
-    <tbody>
-        <tr>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Requested by</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Approved by</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Issued by</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Received by</div>
-            </td>
-        </tr>
-        <tr>
-            <td style="width: 25.0000%;"><br><br><br></td>
-            <td style="width: 25.0000%;"><br></td>
-            <td style="width: 25.0000%;"><br></td>
-            <td style="width: 25.0000%;"><br></td>
-        </tr>
-        <tr>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Name &amp; Position</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Name &amp; Position</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Name &amp; Position</div>
-            </td>
-            <td style="width: 25.0000%;">
-                <div style="text-align: center;">Name &amp; Position</div>
-            </td>
-        </tr>
-    </tbody>
-</table>
+<!-- Data Tables -->
+<link rel="stylesheet" type="text/css" media="screen"
+    href="https://cdn.datatables.net/1.12.0/css/jquery.dataTables.min.css">
+<script src="https://code.jquery.com/jquery-3.5.1.js"> </script>
+<script src="https://cdn.datatables.net/1.12.0/js/jquery.dataTables.min.js"> </script>
 
-<script>
-window.print();
-</script>
+<div class="container" style="margin-top: 10px;">
+    <div class="row ">
+        <div class="col-lg-7 col-md-12">
+            <div class="cards shadow p-3 mb-5 bg-white rounded" style="min-height: 400px;">
+                <div class="card-header card-header-text" style="background-color:#fff">
+                    <h4 class="card-title">Lending Activity</h4>
+                    <!-- <p class="category">All Warehouse</p> -->
+                </div>
+                <div class="card-content table-responsive">
+                    <table id="pre" class="table table-striped table-hover">
+                        <thead class="text-primary">
+                            <tr>
+                                <th>No</th>
+                                <th>Lending No</th>
+                                <th>Date</th>
+                                <th>Item Code</th>
+                                <th>Qty</th>
+                                <th>Dept</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                            </tr>
+
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+
+        <script>
+        $(document).ready(function() {
+            //$('#warehouse').DataTable();
+            $('#lending').DataTable();
+
+        });
+        </script>
+
+
+        </html>
