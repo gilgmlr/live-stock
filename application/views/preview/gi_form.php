1e0b45917d403ed55a780af01bc26a134f144e0f
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-13 15:23:18 +0700

    .

--- application/views/preview/gi_form.php
+++ application/views/preview/gi_form.php
@@ -107,9 +107,9 @@
                     </div>
                 </div>
             </div>
-
-            <!-- ALERT -->
-            <!-- <div class="position-absolute top-50 start-50 translate-middle">
+        </div>
+        <!-- ALERT -->
+        <!-- <div class="position-absolute top-50 start-50 translate-middle">
                 <div class="alert alert-success" style="width : 250px" role="alert">
                     <h4 class="alert-heading">Confirmation Alert!</h4>
                     <p>Are you sure want to save this?.</p>
@@ -119,8 +119,6 @@
 
                 </div>
             </div> -->
-
-        </div>
     </div>
 
 
