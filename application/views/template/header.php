afb13bd18008213876924ae07d2ef292eb95fe92
Gilang Gumelar <gilanggumelar1945@gmail.com>
2022-06-14 13:19:55 +0700

    Icon Web

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -20,6 +20,9 @@
     <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 
+    <!-- Icon  -->
+    <link href="assets/image/icon.png" rel='shorcut icon'>
+
     <!-- IconGoogle -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet"
