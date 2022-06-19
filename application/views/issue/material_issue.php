e79c51157beaa34b41dacaf4fdcab9bbc5f4077e
izalmfrds <rizalmaidanytb@gmail.com>
2022-06-19 12:01:38 +0700

    MI

--- application/views/issue/material_issue.php
+++ application/views/issue/material_issue.php
@@ -14,33 +14,35 @@
             <form action="<?= base_url() ?>issue/addMI" method="POST" autocomplete="off">
                 <div class="container">
                     <div class="row">
-                        <label for="" class="col-sm-6 col-form-label">MI No</label>
-                        <input type="text" class="form-control" id="mi_code" name="mi_code"
-                            value="MI01-C02<?= substr(date('Y'),2,4) . date('m') ?>" required>
-                        <!-- <select class="form-select form-control" aria-label=".form-select-lg example" id="mi_code"
-                                name="mi_code" required>
-                                <option selected>-- Select --</option>
-                                <?php foreach ($mi_code as $data) { ?>
-                                <option value="<?= $data->doc_no ?>"><?= $data->doc_no ?></option>
-                                <?php $i++; } ?>
-                            </select>  -->
+                        <div class="col-sm-6">
+                            <label for="" class="col-sm-6 col-form-label">MI No</label>
+                            <input type="text" class="form-control" id="mi_code" name="mi_code"
+                                value="MI01-C02<?= substr(date('Y'),2,4) . date('m') ?>" required>
+                        </div>
 
-                        <label for="" class="col-sm-6 col-form-label">Entry Date</label>
-                        <input type="date" class="form-control" id="entri_date" name="entri_date"
-                            value="<?php echo date('Y-m-d'); ?>" required>
-                        </select> <label for="" class="col-sm-6 col-form-label">Posting Date</label>
-                        <input type="date" class="form-control" id="post_date" name="post_date"
-                            value="<?php echo date('Y-m-d'); ?>" required>
-
-
-
-                    </div>
-                    <div class="row">
                         <div class="col">
-                            <label for="" class="col-sm-6 col-form-label">Dept No</label>
+                            <label for="" class="col-sm-3 col-form-label">Dept No</label>
                             <input type="text" class="form-control" id="dept_no" name="dept_no" required
                                 autocomplete="off">
+                        </div>
+                        <div class="col">
+                            <label for="" class="col-sm-4 col-form-label">Dept Name</label>
+                            <input type="text" class="form-control" id="dept_name" name="dept_name" required
+                                autocomplete="off">
+                        </div>
 
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <label for="" class="col-sm-6 col-form-label">Entry Date</label>
+                            <input type="date" class="form-control" id="entri_date" name="entri_date"
+                                value="<?php echo date('Y-m-d'); ?>" required>
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="" class="col-sm-6 col-form-label">Posting Date</label>
+                            <input type="date" class="form-control" id="post_date" name="post_date"
+                                value="<?php echo date('Y-m-d'); ?>" required>
                         </div>
                         <div class="col">
                             <label for="" class="col-sm-6 col-form-label">Project No</label>
@@ -48,71 +50,88 @@
                                 autocomplete="off">
                         </div>
                     </div>
+
                     <label for="" class="col-sm-6 col-form-label">Description</label>
                     <textarea class="form-control" id="desc" name="desc" rows="1"></textarea>
 
-                    <div class="col-sm-6">
-
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <label for="" class="col-sm-6 col-form-label">Reference</label>
+                            <input type="text" class="form-control" id="reference" name="reference" required
+                                autocomplete="off">
+                        </div>
+                        <div class="col-sm-3">
+                            <label for="" class="col-sm-6 col-form-label">Reason</label>
+                            <input type="text" class="form-control" id="reason_code" name="reason_code" required
+                                autocomplete="off">
+                        </div>
 
                         <div class="col-sm-6">
-                            <label for="" class="col-sm-6 col-form-label">Item Code</label>
-                            <input type="text" class="form-control" id="item_code" name="item_code" required>
+                            <label for="" class="col-sm-4 col-form-label">Entered by</label>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" id="created_by" name="created_by" readonly
+                                        value="<?= $this->session->userdata('nip'); ?>">
+                                </div>
 
+                                <div class="col-sm-6">
+                                    <input type="text" class="form-control" id="" name="" readonly
+                                        value="<?= $this->session->userdata('name'); ?>">
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-sm-6">
-                            <label for="" class="col-sm-6 col-form-label">Item Name</label>
-                            <input type="text" class="form-control" id="item_name" name="item_name" readonly>
-                        </div>
+                    </div>
 
+                    <div class="col-sm-12">
+                        <hr size="12px">
+                    </div>
+
+
+                    <div class="col-sm-12">
                         <div class="row">
-                            <div class="col-sm-10">
+                            <div class="col-sm-3">
+                                <label for="" class="col-sm-6 col-form-label">Item Code*</label>
+                                <input type="text" class="form-control" id="item_code" name="item_code[]">
+                                <small class="form-text text-danger"><?= form_error('item_code') ?></small>
+
+                            </div>
+                            <div class="col-sm-3">
+                                <label for="" class="col-sm-6 col-form-label">Item Name</label>
+                                <input type="text" class="form-control" id="item_name" name="item_name" readonly>
+                            </div>
+                            <div class="col-sm-5">
                                 <label for="" class="col-sm-6 col-form-label">Spesification</label>
                                 <input type="text" class="form-control" id="specification" name="specification"
                                     readonly>
 
                             </div>
-                            <div class="col-sm-2">
+                            <div class="col-sm-1">
                                 <label for="" class="col-sm-6 col-form-label">UoM</label>
                                 <input type="text" class="form-control" id="uom" name="uom" readonly>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <label for="" class="col-sm-6 col-form-label">Transaction Qty</label>
-                                <input type="text" class="form-control" id="transaction_qty" name="transaction_qty"
-                                    required>
-                            </div>
 
-                            <div class="col-sm-6">
-                                <label for="" class="col-sm-6 col-form-label">Warehouse</label>
-                                <input type="text" class="form-control" id="warehouse_code" name="warehouse_code"
-                                    value="<?= $this->session->userdata('warehouse') ?>" readonly>
-                            </div>
-                        </div>
 
                         <div class="row">
-                            <div class="col">
-                                <label for="" class="col-sm-6 col-form-label">Reference</label>
-                                <input type="text" class="form-control" id="reference" name="reference" required
-                                    autocomplete="off">
+                            <div class="col-sm-2">
+                                <label for="" class="col-sm-6 col-form-label">Qty*</label>
+                                <input type="text" class="form-control" id="qty" name="qty[]">
                             </div>
-                            <div class="col">
-                                <label for="" class="col-sm-6 col-form-label">Reason</label>
-                                <input type="text" class="form-control" id="reason_code" name="reason_code" required
-                                    autocomplete="off">
+                            <div class="col-sm-4">
+                                <label for="" class="col-sm-6 col-form-label">Location*</label>
+                                <input type="text" class="form-control" id="location" name="location[]">
                             </div>
-                        </div>
-                        <label for="" class="col-sm-6 col-form-label">Entered by</label>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" id="created_by" name="created_by" readonly
-                                    value="<?= $this->session->userdata('nip'); ?>">
+                            <div class="col-sm-3">
+                                <label for="" class="col-sm-6 col-form-label">Warehouse*</label>
+                                <input type="text" class="form-control" id="warehouse_code" name="warehouse_code[]"
+                                    value="<?= $this->session->userdata('warehouse') ?>">
                             </div>
-                            <div class="col-sm-6">
-                                <input type="text" class="form-control" id="" name="" readonly
-                                    value="<?= $this->session->userdata('name'); ?>">
+                            <div class="col-sm-3">
+                                <label for="" class="col-sm-6 col-form-label">Equipment</label>
+                                <input type="text" class="form-control" id="equipment" name="equipment[]">
                             </div>
                         </div>
+                        <hr size="12px">
                     </div>
                 </div>
         </div>
